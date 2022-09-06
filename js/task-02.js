e28cd2b6da2621324c6f44aa869bd9a40273fca4
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-09-06 20:00:04 +0300

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ulRef = document.querySelector('ul');
+const ListLi = ingredients.map((element) => {
+  console.log(element);
+  const liRef = document.createElement('li');
+  console.log(liRef);
+  liRef.textContent = element;
+  return liRef;
+});
+ulRef.append(...ListLi);

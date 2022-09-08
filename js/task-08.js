520920060699d761bb014cc7410e9a9dc63f953b
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-09-08 14:03:19 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
+const btnRef = document.querySelector('button');
+// const dataArray ={}
 
+
+formRef.addEventListener('submit', onBtnClick);
+
+function onBtnClick(event) {
+    event.preventDefault();
+    const {elements: {email, password}} = event.target;
+if (email.value === "" || password.value === "") {
+    alert('всі поля повинні бути заповнені');
+    return
+}
+  const dataArray = {
+    email: email.value,
+    password: password.value,
+  }
+  console.log(dataArray);
+  formRef.reset()
+}

917b5742602d275f7a0873551a9cf60ca6ddbf8a
Oleksandr Lytvyn <tvinlee7@gmail.com>
2022-09-09 13:32:51 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('[class="color"]');
+button.addEventListener('click', ()=>{
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+})
